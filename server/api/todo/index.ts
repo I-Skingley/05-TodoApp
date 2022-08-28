b1d8e154903cc3a2b64f28a7f0732f556208425c
I-Skingley <ijskingley@gmail.com>
2022-08-28 13:56:33 +0200

    Push event body - new todo

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,12 +1,23 @@
 import { db } from "../../db"
+import {v4 as uuid} from "uuid"
 
-export default defineEventHandler((e) => {
-    const method = e.req.method;
+export default defineEventHandler(async (e) => {
+    const method = e.req.method
     if(method==="GET"){
         console.log("GET")
-        return db.todos;
+        return db.todos
     }
-    else if(method==="POST"){
-        console.log("POST")
+    if(method==="POST"){
+        const body = await useBody(e)
+        if(!body.item) throw new Error()
+
+        const newTodo = {
+            id: uuid(),
+            item: body.item,
+            completed: false,
+        }
+        db.todos.push(newTodo)
+
+        return newTodo
     }
 })
