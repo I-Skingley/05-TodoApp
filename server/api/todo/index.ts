1d36081a1275c828cc2bb998bc6b74b86bdd9bab
I-Skingley <ijskingley@gmail.com>
2022-08-28 13:45:05 +0200

    Check event req method

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,3 +1,12 @@
-export default defineEventHandler(() => {
-    return "Hello this is data";
+import { db } from "../../db"
+
+export default defineEventHandler((e) => {
+    const method = e.req.method;
+    if(method==="GET"){
+        console.log("GET")
+        return db.todos;
+    }
+    else if(method==="POST"){
+        console.log("POST")
+    }
 })
